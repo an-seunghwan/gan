651164e28c91a4276083c3b35987db33d69a5433
an-seunghwan <dpeltms79@gmail.com>
2021-07-08 13:29:56 +0900

    Update model_cmnist4.py

--- src/modules/model_cmnist4.py
+++ src/modules/model_cmnist4.py
@@ -8,22 +8,26 @@ def build_encoder(PARAMS):
     x = layers.Input((PARAMS['data_dim'], PARAMS['data_dim'], PARAMS['channel']))
     noise = layers.Input((PARAMS['latent_dim']))
     
-    h = layers.Conv2D(filters = 16, kernel_size = 3, strides=2, activation='relu', padding = 'same')(x)
+    h = layers.Conv2D(filters = 16, kernel_size = 3, strides=2, padding = 'same')(x)
     h = layers.BatchNormalization()(h)
-    h = layers.Conv2D(filters = 32, kernel_size = 3, strides=2, activation='relu', padding = 'same')(h)
+    h = layers.LeakyReLU(0.2)(h)    
+    h = layers.Conv2D(filters = 32, kernel_size = 3, strides=2, padding = 'same')(h)
     h = layers.BatchNormalization()(h)
+    h = layers.LeakyReLU(0.2)(h)
     
     h = layers.Flatten()(h)
     h = layers.Dense(512)(h)
     h = layers.LeakyReLU(0.2)(h)
     h = layers.Dense(PARAMS['latent_dim'])(h)
+    h = layers.LeakyReLU(0.2)(h)
     
     noise_h = layers.Dense(PARAMS['latent_dim'])(noise)
+    noise_h = layers.LeakyReLU(0.2)(noise_h)
     
     h = h * noise_h
     
     E = K.models.Model([x, noise], h)
-    E.summary()
+    # E.summary()
     
     return E
 #%%
@@ -46,36 +50,29 @@ def build_generator(PARAMS):
     
     h = tf.reshape(h, [-1, 8, 8, 32])
     h = layers.Conv2D(filters = 32, kernel_size = 3, padding = 'same')(h)
+    h = layers.LeakyReLU(0.2)(h)
     h = layers.UpSampling2D(size=(2, 2), interpolation='bilinear')(h)
     h = layers.Conv2D(filters = 16, kernel_size = 3, padding = 'same')(h)
+    h = layers.LeakyReLU(0.2)(h)
     h = layers.UpSampling2D(size=(2, 2), interpolation='bilinear')(h)
     
     h = layers.Conv2D(filters = 3, kernel_size = 1, activation='sigmoid', padding = 'same')(h)
     
     G = K.models.Model([z, y], h)
-    G.summary()
-#%%
-def dis_block(x, filters, pooling = True):
-    h = layers.Conv2D(filters = filters, kernel_size = 3, padding = 'same')(x)
-    h = layers.LeakyReLU(0.2)(h)
-    if pooling:
-        h = layers.AveragePooling2D()(h)
-    return h
+    # G.summary()
+    
+    return G
 #%%
 def build_image_discriminator(PARAMS):
     x = layers.Input([PARAMS['data_dim'], PARAMS['data_dim'], PARAMS['channel']])
 
-    dim = 16
-
-    h = dis_block(x, 1 * dim)  
-    h = dis_block(h, 2 * dim) 
-    h = dis_block(h, 4 * dim)
-    h = dis_block(h, 8 * dim) 
-    h = dis_block(h, 16 * dim) 
-
+    h = layers.Conv2D(filters = 8, kernel_size = 3, strides = 2, padding = 'same')(x)
+    h = layers.LeakyReLU(0.2)(h)
     h = layers.Flatten()(h)
 
-    h = layers.Dense(4 * dim)(h)
+    h = layers.Dense(512)(h)
+    h = layers.LeakyReLU(0.2)(h)
+    h = layers.Dense(128)(h)
     h = layers.LeakyReLU(0.2)(h)
 
     h = layers.Dense(1, activation='sigmoid')(h)
@@ -88,19 +85,13 @@ def build_image_discriminator(PARAMS):
 def build_image_classifier(PARAMS):
     x = layers.Input([PARAMS['data_dim'], PARAMS['data_dim'], PARAMS['channel']])
 
-    dim = 16
-
-    h = dis_block(x, 1 * dim)  
-    h = dis_block(h, 2 * dim) 
-    h = dis_block(h, 4 * dim)
-    h = dis_block(h, 8 * dim) 
-    h = dis_block(h, 16 * dim) 
-
+    h = layers.Conv2D(filters = 8, kernel_size = 3, strides = 2, padding = 'same')(x)
+    h = layers.LeakyReLU(0.2)(h)
     h = layers.Flatten()(h)
 
-    h = layers.Dense(8 * dim)(h)
+    h = layers.Dense(512)(h)
     h = layers.LeakyReLU(0.2)(h)
-    h = layers.Dense(4 * dim)(h)
+    h = layers.Dense(128)(h)
     h = layers.LeakyReLU(0.2)(h)
 
     h = layers.Dense(PARAMS['class_num'], activation='softmax')(h)
@@ -113,15 +104,15 @@ def build_image_classifier(PARAMS):
 def build_z_discriminator(PARAMS):
     x = layers.Input([PARAMS['latent_dim']])
 
-    h = layers.Dense(128)(x)
+    h = layers.Dense(1024)(x)
+    h = layers.LeakyReLU(0.2)(h)
+    h = layers.Dense(512)(h)
+    h = layers.LeakyReLU(0.2)(h)
+    h = layers.Dense(256)(h)
+    h = layers.LeakyReLU(0.2)(h)
+    h = layers.Dense(128)(h)
     h = layers.LeakyReLU(0.2)(h)
     h = layers.Dense(64)(h)
-    h = layers.LeakyReLU(0.2)(h)
-    h = layers.Dense(32)(h)
-    h = layers.LeakyReLU(0.2)(h)
-    h = layers.Dense(16)(h)
-    h = layers.LeakyReLU(0.2)(h)
-    h = layers.Dense(8)(h)
     h = layers.LeakyReLU(0.2)(h)
 
     h = layers.Dense(1, activation='sigmoid')(h)
